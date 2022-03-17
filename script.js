c36c691990c5a23127a524ecc2632e97d3b798b8
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2022-03-17 23:50:38 +0100

    functionality added

---
+++ script.js
@@ -0,0 +1,25 @@
+const multiStep = document.querySelector("[data-multi-step]")
+const formStep = [...multiStep.querySelectorAll("[data-step]")]
+
+let currentStep = formStep.findIndex(step => {
+  return step.classList.contains("active")
+})
+if (currentStep < 0) {
+  currentStep = 0
+  showCurrentStep()
+}
+
+multiStep.addEventListener("click", e => {
+  if (e.target.matches("[data-next]")) {
+    currentStep += 1
+  } else if (e.target.matches("[data-previous]")) {
+    currentStep -= 1
+  }
+  showCurrentStep()
+})
+
+function showCurrentStep() {
+  formStep.forEach((step, index) => {
+    step.classList.tog('active'.index === currentStep)
+  })
+}
