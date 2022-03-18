c3781f2721a7b91a2ef537f3440913c44869960a
Abeeb Ridwan Olumide <igortunde@yahoo.com>
2022-03-18 11:44:47 +0100

    logic refactored to correct mistakes

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const formStep = [...multiStep.querySelectorAll("[data-step]")]
 let currentStep = formStep.findIndex(step => {
   return step.classList.contains("active")
 })
+
 if (currentStep < 0) {
   currentStep = 0
   showCurrentStep()
@@ -14,12 +15,14 @@ multiStep.addEventListener("click", e => {
     currentStep += 1
   } else if (e.target.matches("[data-previous]")) {
     currentStep -= 1
+  } else {
+    return
   }
   showCurrentStep()
 })
 
 function showCurrentStep() {
   formStep.forEach((step, index) => {
-    step.classList.tog('active'.index === currentStep)
+    step.classList.toggle('active', index === currentStep)
   })
 }
